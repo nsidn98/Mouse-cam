b1d5c0830dd9420516d67da6482e6cb92dc896cb
Siddharth Nayak <siddharthnayak98@gmail.com>
2017-10-28 22:18:55 +0530

    Add files via upload

---
+++ Mouse_Gyro_ENG.ino
@@ -0,0 +1,49 @@
+/* Code to control the mouse pointer
+ * through the movement of the head
+ * Change values at vx and vy (+300 and -100 in my case)
+ * using the TEST code to make your project work.
+ * 
+ * Gabry295
+ */
+
+#include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
+//#include <Mouse.h>
+
+MPU6050 mpu;
+int16_t ax, ay, az, gx, gy, gz;
+int vx, vy;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  mpu.initialize();
+  if (!mpu.testConnection()) {
+    while (1);
+    }
+}
+ void prt(int a,int b,int c,int d,int e,int f)
+ {
+  Serial.print(a);
+  Serial.print("   ");
+  Serial.print(b);
+  Serial.print("   ");
+  Serial.print(c);
+  Serial.print("   ");
+  Serial.print(d);
+  Serial.print("   ");
+  Serial.print(e);
+  Serial.print("   ");
+  Serial.println(f);
+ }
+void loop() {
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+ // vx = -(gx+300)/80;  // "+300" because the x axis of gyroscope give values about -350 while it's not moving. Change this value if you get something different using the TEST code, chacking if there are values far from zero.
+  //vy = (gy)/80; // same here about "-100"
+  prt(ax,ay,az,gx,gy,gz);
+//  Mouse.move(vx, vy);
+  
+  delay(20);
+}
---
+++ mouse.py
@@ -0,0 +1,56 @@
+import serial
+import re
+import time
+import pyautogui
+pyautogui.FAILSAFE = False 
+
+ser =serial.Serial('/dev/cu.usbmodem1421',9600,timeout=None)
+x=0
+y=0
+i=0
+while 1:
+	i+=1
+	try:
+		a=ser.readline()
+		c=a.decode('ASCII')
+
+		#time.sleep(0.01)
+	except ser.SerialTimeoutException:
+		print('Data could not be read')
+		time.sleep(1)
+	b=c.split()
+	print(b)
+	
+	if(i<3):
+		continue
+	else:
+		if(len(b)==2):
+			x=int(b[0])
+			y=int(b[1])
+		elif(len(b)==3):				
+			'''if len(b==3)  b=[button_click,x,y]  button_click='a','b'
+			'a'=left-click
+			'b'=right-click'''
+			if b[0]=='a':
+				pyautogui.click(button='left')
+			elif b[0]=='b':
+				pyautogui.click(button='right')
+		elif(len(b)==4):
+			'''b=[scroll_button,x,y,scroll_amount]
+			'c'=vertical scroll
+			'd'=horizontal scroll'''
+			scroll_amount=int(b[3])
+			if(b[0]=='c'):
+				pyautogui.scroll(scroll_amount)
+			elif(b[0]=='d'):
+				pyautogui.hscroll(scroll_amount)
+
+	pyautogui.moveRel(x,y,duration=0)
+	
+					
+
+
+
+
+			
+
